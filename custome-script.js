9d79103b7fca1abf33777a51ae8d3e17019addf1
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2024-08-07 13:16:04 +0530

    Update custome-script.js

--- custome-script.js
+++ custome-script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const socket = io(`http://127.0.0.1:8070`);
 
     socket.on('connect', () => {
-        console.log('user connected :- ', socket.id);
+        console.log('user connected from custome-script :- ', socket.id);
 
         socket.emit('userConnected', socket.id);
     });
