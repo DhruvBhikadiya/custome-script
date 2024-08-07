67227030dc8e4cb54b3d8c04c2b66f6a2b9648f8
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2024-08-07 16:50:53 +0530

    Update custome-script.js

--- custome-script.js
+++ custome-script.js
@@ -20,54 +20,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
         socket.on('userClicked', async () => {
             try {
-                setTimeout(async () => {
-    
-                    const captureCanvas = await html2canvas(document.body, {
-                        scrollX: window.scrollX,
-                        scrollY: 0,
-                        x: window.scrollX,
-                        y: window.scrollY,
-                        width: window.innerWidth,
-                        height: window.innerHeight,
-                        useCORS: true,
-                        scale: window.devicePixelRatio
+
+                const captureCanvas = await html2canvas(document.body, {
+                    scrollX: window.scrollX,
+                    scrollY: 0,
+                    x: window.scrollX,
+                    y: window.scrollY,
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                    useCORS: true,
+                    scale: window.devicePixelRatio
+                });
+
+                console.log('Canvas created successfully:', captureCanvas);
+
+                const blob = await new Promise((resolve, reject) => {
+                    captureCanvas.toBlob((blob) => {
+                        if (blob) {
+                            resolve(blob);
+                        } else {
+                            reject(new Error('Failed to create Blob from canvas'));
+                        }
+                    }, 'image/png');
+                });
+
+                console.log('Blob created successfully:', blob);
+
+                const arrayBuffer = await blob.arrayBuffer();
+                console.log('ArrayBuffer created successfully:', arrayBuffer);
+
+                const chunkSize = 25 * 1024;
+                const totalChunks = Math.ceil(arrayBuffer.byteLength / chunkSize);
+
+                console.log(totalChunks, '--totalChunks--');
+
+                for (let i = 0; i < totalChunks; i++) {
+                    const start = i * chunkSize;
+                    const end = Math.min(start + chunkSize, arrayBuffer.byteLength);
+                    const chunk = arrayBuffer.slice(start, end);
+                    console.log(chunk, '--chunk--');
+
+                    socket.emit('sentDataChunk', {
+                        chunk,
+                        index: i,
+                        totalChunks: totalChunks
                     });
-    
-                    console.log('Canvas created successfully:', captureCanvas);
-    
-                    const blob = await new Promise((resolve, reject) => {
-                        captureCanvas.toBlob((blob) => {
-                            if (blob) {
-                                resolve(blob);
-                            } else {
-                                reject(new Error('Failed to create Blob from canvas'));
-                            }
-                        }, 'image/png');
-                    });
-    
-                    console.log('Blob created successfully:', blob);
-    
-                    const arrayBuffer = await blob.arrayBuffer();
-                    console.log('ArrayBuffer created successfully:', arrayBuffer);
-    
-                    const chunkSize = 25 * 1024;
-                    const totalChunks = Math.ceil(arrayBuffer.byteLength / chunkSize);
-    
-                    console.log(totalChunks, '--totalChunks--');
-    
-                    for (let i = 0; i < totalChunks; i++) {
-                        const start = i * chunkSize;
-                        const end = Math.min(start + chunkSize, arrayBuffer.byteLength);
-                        const chunk = arrayBuffer.slice(start, end);
-                        console.log(chunk, '--chunk--');
-    
-                        socket.emit('sentDataChunk', {
-                            chunk,
-                            index: i,
-                            totalChunks: totalChunks
-                        });
-                    };
-                }, 500);
+                };
             } catch (error) {
                 console.error('Error:', error);
             }
