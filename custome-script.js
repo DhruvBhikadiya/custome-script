58be6885fe04099eb4abc68875eb286a939574dd
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2024-08-07 13:51:46 +0530

    Update custome-script.js

--- custome-script.js
+++ custome-script.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    <script defer src="https://cdn.socket.io/4.0.0/socket.io.min.js"></script>
-
     const socket = io(`http://127.0.0.1:8070`);
 
     socket.on('connect', () => {
