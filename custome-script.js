6a03d4bcf019528d9be82f88c76b3b7ee0d5e0b3
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2024-10-16 16:04:01 +0530

    Update custome-script.js

--- custome-script.js
+++ custome-script.js
@@ -1,4 +1,5 @@
-const socket = io('https://widget.scoreconnect.com/');
+// const socket = io('https://widget.scoreconnect.com/');
+const socket = io('http://localhost:7070/');
 
 // const socket = io('https://4bbe-2405-201-201c-51bc-f806-b75a-6581-bf5b.ngrok-free.app/', {
 //     extraHeaders: {
